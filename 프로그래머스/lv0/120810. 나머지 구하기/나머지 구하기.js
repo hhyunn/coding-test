a6b6a13218c3cad1c6761f34bbfbfdeb92715454
hhyunn <110486454+hhyunn@users.noreply.github.com>
2023-07-10 01:04:03 +0900

    [level 0] Title: 나머지 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.js
@@ -0,0 +1,13 @@
+/*
+function solution(num1, num2) {
+    let answer = -1;
+    answer = num1 % num2;
+    return answer;
+}
+*/
+
+const solution = (num1, num2) => {
+    let answer = 0;
+    answer = num1 % num2;
+    return answer;
+}
