f185af32de26909c23a96417183ca3de58fad17a
hhyunn <110486454+hhyunn@users.noreply.github.com>
2023-07-10 01:02:45 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,22 @@
+/*
+function solution(num1, num2) {
+    let answer = 0;
+    if(num1 === num2){
+        answer = 1;
+    }
+    else{
+        answer = -1;
+    }
+    return answer;
+}
+*/
+
+const solution = (num1, num2) => {
+    let answer = 0;
+    if (num1 == num2) {
+        answer = 1;
+    } else {
+        answer = -1;
+    }
+    return answer;
+}
