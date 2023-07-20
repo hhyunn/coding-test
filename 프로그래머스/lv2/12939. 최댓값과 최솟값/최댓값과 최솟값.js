b0839bc63ad7e6f95b65728bd11cd7af05091a2c
hhyunn <110486454+hhyunn@users.noreply.github.com>
2023-07-21 01:43:23 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -2,7 +2,7 @@ function solution(s) {
     let answer = '';
     let num = 0;
     num = s.split(' ');
-    num = num.map(x => Number(x));
-    answer += Math.min(...num) + ' ' + Math.max(...num);
+    // num = num.map(x => Number(x));
+    answer = Math.min(...num) + ' ' + Math.max(...num);
     return answer;
 }
