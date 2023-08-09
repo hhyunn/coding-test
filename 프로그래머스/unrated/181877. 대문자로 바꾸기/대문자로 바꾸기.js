9fe2bd55d88677088576047c13321a47675ce7eb
hhyunn <110486454+hhyunn@users.noreply.github.com>
2023-08-09 15:02:00 +0900

    [unrated] Title: 대문자로 바꾸기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.js
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.js
@@ -1,5 +1,6 @@
 function solution(myString) {
     let answer = '';
-    answer = myString.split('').map(v => v.toUpperCase()).join('')
+    // answer = myString.split('').map(v => v.toUpperCase()).join('');
+    answer = myString.toUpperCase();
     return answer;
 }
