573700505e9b9e6ede1931357daf9c84161cf12e
hhyunn <110486454+hhyunn@users.noreply.github.com>
2023-11-18 09:21:39 +0900

    [level unrated] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -0,0 +1,7 @@
+function solution(num_list, n) {
+    const answer = [];
+    for (let i=0; i<num_list.length/n; i++) {
+        answer.push(num_list.slice(n*i, n*(i+1)));
+    }
+    return answer;
+}
