516aa8f30c7457c7a0856df182ce89f49f2ade89
hhyunn <110486454+hhyunn@users.noreply.github.com>
2023-12-03 21:10:54 +0900

    [level 1] Title: 2016년, Time: 0.29 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,4 +1,4 @@
 function solution(a, b) {
-    const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-    return days[new Date(`2016-${a}-${b}`).getDay()];
+    const date = new Date(`2016-${a}-${b}`);
+    return date.toString().slice(0,3).toUpperCase();
 }
