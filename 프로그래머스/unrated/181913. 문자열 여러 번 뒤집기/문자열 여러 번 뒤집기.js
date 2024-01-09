88e1bb5c29bb1262fd27d0c9fa69dbe8de8a15ee
hhyunn <110486454+hhyunn@users.noreply.github.com>
2024-01-09 23:23:46 +0900

    [level unrated] Title: 문자열 여러 번 뒤집기, Time: 8.81 ms, Memory: 35 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -0,0 +1,7 @@
+function solution(my_string, queries) {
+    queries.forEach(v => {
+        const [s, e] = v;
+        my_string = my_string.slice(0, s) + my_string.slice(s, e+1).split('').reverse().join('') + my_string.slice(e+1);
+    })
+    return my_string;
+}
