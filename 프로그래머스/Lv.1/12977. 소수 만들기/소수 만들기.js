a2a6d6fa65036ca3083f70df8a3c9b34b7f20292
hhyunn <110486454+hhyunn@users.noreply.github.com>
2024-01-14 21:04:25 +0900

    [level Lv.1] Title: 소수 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/Lv.1/12977. 소수 만들기/소수 만들기.js
@@ -1,20 +1,21 @@
+function isPrime(sum) {
+    for (let l=2; l<=Math.sqrt(sum); l++) {
+        if (sum % l === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 function solution(nums) {
-    let answer = 0;
+    let count = 0;
     for (let i=0; i<nums.length; i++) {
         for (let j=i+1; j<nums.length; j++) {
             for (let k=j+1; k<nums.length; k++) {
                 const sum = nums[i] + nums[j] + nums[k];
-                const decimal = () => {
-                    for (let l=2; l<=Math.sqrt(sum); l++) {
-                        if (sum % l === 0) {
-                            return false;
-                        }
-                    }
-                    return true;
-                }
-                decimal() ? answer++ : answer;
+                isPrime(sum) ? count++ : count;
             }
         }
     }  
-    return answer;
+    return count;
 }
