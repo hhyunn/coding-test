f368095287969e2237fc270eb0af73dc60d69792
hhyunn <110486454+hhyunn@users.noreply.github.com>
2024-01-15 22:53:36 +0900

    [level Lv.1] Title: 실패율, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/42889. 실패율/실패율.js
@@ -0,0 +1,25 @@
+function solution(N, stages) {
+    let arr = Array.from({ length: N }, () => 0);
+    for (let idx=1; idx<=N; idx++) {
+        for (let j=0; j<stages.length; j++) {
+            if (stages[j] === idx || (stages[j] < idx && stages[j] >= idx) ) {
+                arr[idx-1]++;
+            }
+        }
+    }
+
+    let failRates = [];
+    let leng = stages.length;
+    arr.forEach((v, i) => {
+        if (i === 0) {
+            failRates.push(v / leng);
+        } else {
+            failRates.push(v / (leng -= arr[i-1]));
+        }
+    })
+
+    const answer = failRates.map((failRate, idx) => ({ failRate, idx }))
+                            .sort((a, b) => b.failRate - a.failRate)
+                            .map(({ idx }) => idx+1);
+    return answer;
+}
