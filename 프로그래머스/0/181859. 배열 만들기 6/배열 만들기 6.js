a772cd08ce9322f59eeb26c6e436bd111b2b8721
hhyunn <110486454+hhyunn@users.noreply.github.com>
2024-02-02 16:46:41 +0900

    [level 0] Title: 배열 만들기 6, Time: 3.44 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -1,12 +1,10 @@
 function solution(arr) {
     const stk = [];
     for (let i=0; i<arr.length; i++) {
-        if (!stk.length) {
+        if (!stk.length || stk[stk.length - 1] !== arr[i]) {
             stk.push(arr[i]);
-        } else if (stk.length && stk[stk.length - 1] === arr[i]) {
+        } else {
             stk.pop();
-        } else if (stk.length && stk[stk.length - 1] !== arr[i]) {
-            stk.push(arr[i]);
         }
     }
     return stk.length ? stk : [-1];
