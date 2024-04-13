7c7c03a2a6ec4365c939154a40b175a566e5d602
hhyunn <110486454+hhyunn@users.noreply.github.com>
2024-04-13 20:59:26 +0900

    [level 0] Title: 배열 만들기 4, Time: 4.32 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    stk = []
+    i = 0
+    while (i < len(arr)):
+        if stk != [] and stk[-1] >= arr[i]:
+            del stk[-1]
+        else:
+            stk.append(arr[i])
+            i += 1
+    return stk
